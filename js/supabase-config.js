9e58dba1c431ef995d9d51ff5982ddb1cbeff8ad
saulecast <saulecast@gmail.com>
2026-06-29 12:28:41 +0000

    push notification 1ra version lista

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -3,4 +3,4 @@ window.BCC_SUPABASE = {
   anonKey: "sb_publishable_X_3U_TNtC9BuVwc-vMCsug_GVFmI5cQ"
 };
 
-window.BCC_WEB_PUSH_PUBLIC_KEY = "";
+window.BCC_WEB_PUSH_PUBLIC_KEY = "BL7ZY6d49L451BwhDIqFa0dSPdXm1kIfxrQXImw2ZPAYxNPgJ64NuaVsM8JN01ZJKLEBHadNS_F0ZiCqs6Izk3c";
